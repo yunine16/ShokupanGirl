a1495854a400e63f10983de9a8405f7e2334d62a
yunine16 <yuchin616@icloud.com>
2019-04-28 16:27:06 +0900

    FirstCommit

---
+++ ShokupanGirl/Assets/Script/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 0.5f;
+    public int jump = 6;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        //何も押さないと走る、スペースキー押すと止まる
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.zero;
+        }
+        else
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+
+        //エンターキー押すとジャンプ
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jump, 0);
+            Debug.Log(1);
+        }
+
+        
+	}
+}
---
+++ ShokupanGirl/Assets/Script/TimeScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour {
+
+    public Text hourText;
+    public Text minText;
+    public int hour;
+    public int min;
+    private float timeLeft;
+
+	// Use this for initialization
+	void Start () {
+        hour = 7;
+        min = 20;
+        hourText.text = hour.ToString();
+        minText.text = min.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //10秒ごとに処理
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft <= 0.0)
+        {
+            timeLeft = 3.0f;
+            min += 5;
+        }
+
+        //60分になったら時間に１を足す
+        if (min == 60)
+        {
+            min = 0;
+            hour += 1;
+        }
+        
+
+        minText.text = min.ToString();
+        hourText.text = hour.ToString();
+
+	}
+}
