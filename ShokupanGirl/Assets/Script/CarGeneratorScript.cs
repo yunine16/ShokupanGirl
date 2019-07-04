e13e028592f568899b5040c1c43ed4d6d051850a
yunine16 <yuchin616@icloud.com>
2019-07-05 00:42:35 +0900

    車の自動生成機能を追加

---
+++ ShokupanGirl/Assets/Script/CarGeneratorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGeneratorScript : MonoBehaviour {
+
+    public GameObject car;
+    GameObject carGenerator;
+
+    float timeOut;
+    float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+        timeOut = Random.Range(1.0f, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+            Instantiate(car, transform.position, transform.rotation);
+            timeElapsed = 0;
+
+            timeOut = Random.Range(1.0f, 5.0f);
+        }
+	}
+}
