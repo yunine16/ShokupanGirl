de9392e485568f430a97187b1fef2d348a0486da
yunine16 <yuchin616@icloud.com>
2019-07-05 23:54:25 +0900

    男子高校生自動精製機能追加

---
+++ ShokupanGirl/Assets/Script/EnemyGeneratorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGeneratorScript : MonoBehaviour {
+
+    public GameObject[] Enemy;
+    GameObject enemyGenerator;
+
+    float timeOut;
+    float timeElapsed;
+
+    Vector3 pos;
+    public float[] positionX;
+
+	// Use this for initialization
+	void Start () {
+        timeOut = Random.Range(1.0f, 4.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+            enemyGenerator = Enemy[Random.Range(0, Enemy.Length)];
+
+            pos = transform.position;
+            pos.x = positionX[Random.Range(0, positionX.Length)];
+            transform.position = pos;
+
+            Instantiate(enemyGenerator, transform.position, transform.rotation);
+
+            timeElapsed = 0.0f;
+        }
+	}
+}
